98b653b646b0f287896763a5830d02fe7ca3c118
Ryan Mills <millselonius@gmail.com>
2020-08-13 22:34:38 -0600

    game controls

--- script.js
+++ script.js
@@ -6,45 +6,80 @@ Rules:
 
 // Score addition
 
-let roundScore = 0
+let scores, roundScore, activePlayer, gamePlaying
 
+init()
+
+function init () {
+  scores = [0, 0]
+  roundScore = 0
+  activePlayer = 0
+  gamePlaying = true
+}
+
+function nextPlayer () {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  roundScore = 0
+
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+}
+
+// Check for win condition
+function checkWin () {
+  if (scores[activePlayer] > 30) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner'
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active')
+    gamePlaying = false
+  } else {
+    nextPlayer()
+  }
+}
+
+// Add to score
 document.querySelector('.add0-1').addEventListener('click', function () {
   roundScore += 1
-  nextPlayer()
+  checkWin()
   console.log('+1')
+  scores[activePlayer] += roundScore
 })
 
 document.querySelector('.add0-2').addEventListener('click', function () {
   roundScore += 2
-  nextPlayer()
   console.log('+2')
+  document.querySelector('#score-0').textContent = roundScore
 })
 
 document.querySelector('.add0-3').addEventListener('click', function () {
   roundScore += 3
-  nextPlayer()
   console.log('+3')
+  document.querySelector('#score-0').textContent = roundScore
 })
 
 document.querySelector('.add1-1').addEventListener('click', function () {
   roundScore += 1
-  nextPlayer()
   console.log('+1')
+  document.querySelector('#score-1').textContent = roundScore
 })
 
 document.querySelector('.add1-2').addEventListener('click', function () {
   roundScore += 2
-  nextPlayer()
   console.log('+2')
+  document.querySelector('#score-1').textContent = roundScore
 })
 
 document.querySelector('.add1-3').addEventListener('click', function () {
   roundScore += 3
-  nextPlayer()
   console.log('+3')
+  document.querySelector('#score-1').textContent = roundScore
 })
 
-function nextPlayer () {
-  console.log('Next player working')
-  console.log(roundScore)
-}
+// Check for win condition
+
+document.querySelector('.new-button').addEventListener('click', function () {
+  init()
+})
